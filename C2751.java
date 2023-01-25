26cefc6658fd2695245516d56c295b9da98b38fa
hsnkch <chsick@naver.com>
2023-01-25 22:24:05 +0900

    20230125

---
+++ C2751.java
@@ -0,0 +1,24 @@
+package study;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int n = Integer.parseInt(br.readLine());
+		int a[] = new int[n];
+		for (int i = 0; i < n; i++) {
+			a[i] = Integer.parseInt(br.readLine());
+		}
+		Arrays.sort(a);
+		for (int asd : a) {
+			sb.append(asd).append('\n');
+		}
+		System.out.println(sb);
+	}
+}
