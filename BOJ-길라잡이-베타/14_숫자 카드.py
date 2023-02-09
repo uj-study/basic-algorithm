7fef9de6deeba36950c7631b8f947cc505209184
hsnkch <chsick@naver.com>
2023-02-09 21:05:21 +0900

    Update 14_숫자 카드.py

--- BOJ-길라잡이-베타/14_숫자 카드.py
+++ BOJ-길라잡이-베타/14_숫자 카드.py
@@ -1,26 +1,25 @@
 import sys
 input = sys.stdin.readline
 n = int(input())
-a = list(map(int, input().split())).sort()
+a = sorted(list(map(int, input().split())))
 m = int(input())
 b = list(map(int, input().split()))
 
 count = 0
+def search(first, last, a, r):
+    mid = (first + last) // 2
+    if r == a[mid]:
+        return a[first:last+1].count(r)
+    elif r > a[mid]:
+        return search(mid+1,last,a,r)
+    else:
+        return search(first,mid - 1,a,r)
 
-for i in b:
+q = {}
+
+for r in a:
     first = 0
     last = n - 1
-
-    while first <= last:
-        mid = (first + last) / 2
-        if i == a[mid]:
-            count += 1
-        elif i > a[mid]:
-            first = mid + 1
-        else:
-            last = mid - 1
-    
-    if count == 0:
-        print(0)
-    else:
-        print(count)
+    if r not in q:
+        q[r] = search(first,last,a,r)
+print(' '.join(str(q[x]) if x in q else '0' for x in b))
