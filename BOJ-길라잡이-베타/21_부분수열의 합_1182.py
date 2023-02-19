c1cb895ffa40f5910b2b63791bee6e2b1dbd2fd1
hsnkch <chsick@naver.com>
2023-02-19 17:36:29 +0900

    230219

--- BOJ-길라잡이-베타/21_부분수열의 합_1182.py
+++ BOJ-길라잡이-베타/21_부분수열의 합_1182.py
@@ -2,6 +2,15 @@ from itertools import combinations
 import sys
 input = sys.stdin.readline
 
-N, S = input().split()
+N, S = map(int,input().split())
 a = list(map(int,input().split()))
+count = 0
 
+for no in range(1, N+1):
+    a_com = combinations(a, no)
+    
+    for i in a_com:
+        if sum(i) == S:
+            count += 1
+
+print(count)
