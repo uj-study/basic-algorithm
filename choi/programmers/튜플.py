ca2b5ef241e68bb7afa611a80dce12d67c872084
hsnkch <chsick@naver.com>
2023-04-23 20:20:23 +0900

    수정

--- choi/programmers/튜플.py
+++ choi/programmers/튜플.py
@@ -1,16 +1,19 @@
 def solution(s):
     sList = s[2:-2].split('},{')
     sList.sort(key=lambda x: len(x))
-    last_index = len(sList[-1])//2+1
+    
     for i in range(len(sList)):
         sList[i] = sList[i].split(',')
+    
     ans=[]
+
     for i in sList[-1]:
-        if i != ',':
-            for j in range(len(sList)):
-                if i in sList[j]:
-                    ans.append(int(i))
+        for j in range(len(sList)):
+            if i in sList[j]:
+                ans.append(int(i))
+
     count_ans = {}
+    
     for i in ans:
         count_ans[i] = count_ans.get(i,0)+1
 
