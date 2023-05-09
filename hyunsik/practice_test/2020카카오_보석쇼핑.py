a0018ddbf49b903c3dc078071124ef25a501106a
hyunsik <hyunsig96@gmail.com>
2023-05-09 16:00:18 +0900

    fix: 2020카카오 보석쇼핑

--- hyunsik/practice_test/2020카카오_보석쇼핑.py
+++ hyunsik/practice_test/2020카카오_보석쇼핑.py
@@ -1,17 +1,45 @@
 def solution(gems):
-    dt = {}
-    for i in range(len(gems)):
-        if dt.get(gems[i]):
-            dt[gems[i]][0] = min(dt[gems[i]][0], i)
-            dt[gems[i]][1] = max(dt[gems[i]][1], i)
-        else:
-            dt[gems[i]] = [i, i] # 최소와 최대만 저장
-    ans_0 = 100001
-    ans_1 = 0
-    for _, x in dt.items():
-        ans_0 = min(ans_0, x[1])
-        ans_1 = max(ans_1, x[0])
+    N_gems = len(gems) # 보석 총 수
+    set_gems = set(gems) # 보석 종류
+    N_set = len(set_gems) # 보석 종류의 수
+    lp, rp = 0, N_set - 1
+    dic = {}
+    ans = [0, N_gems - 1]
+
+    for i in range(lp, rp+1):
+        dic[gems[i]] = dic.get(gems[i], 0) + 1
     
-    return [ans_0 + 1, ans_1 + 1]
+    while lp<N_gems and rp<N_gems:
+        if len(dic) < N_set:
+            rp += 1
+            if rp == N_gems:
+                break
+            dic[gems[rp]] = dic.get(gems[rp], 0) + 1
+        else:
+            if ans[1] - ans[0] > rp - lp:
+                ans[0], ans[1] = lp, rp
+            dic[gems[lp]] -= 1
+            if dic[gems[lp]] == 0:
+                del dic[gems[lp]]
+            lp += 1
+    return [ans[0]+1, ans[1]+1]
+
+
+# 시간초과
+# def solution(gems):
+#     N_gems = len(gems) # 보석 총 수
+#     set_gems = set(gems) # 보석 종류
+#     N_set = len(set_gems) # 보석 종류의 수
+#     lp, rp = 0, N_set - 1
+#     ans = [0, N_gems - 1]
+
+#     while lp<N_gems and rp<N_gems:
+#         if set_gems == set(gems[lp:rp+1]):
+#             if ans[1] - ans[0] > rp - lp:
+#                 ans[0], ans[1] = lp, rp
+#             lp += 1
+#         else:
+#             rp += 1
+#     return [ans[0]+1, ans[1]+1]
 
 print(solution(["AA", "AB", "AC", "AA", "AC"]))
